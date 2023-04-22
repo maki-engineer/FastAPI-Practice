9717ecef68fe81d841daf47563da5d4773a6e146
maki-engineer <ryohei-81@outlook.jp>
2023-04-22 10:17:43 +0900

    update: Enumを使ったモデルを使用してのAPI作成

--- main.py
+++ main.py
@@ -23,3 +23,13 @@ def read_user_me():
 @app.get("/users/{user_id}")
 def read_user(user_id: int):
   return {"user_id": user_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+  if model_name is ModelName.alexnet:
+    return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+  if model_name.value == "lenet":
+    return {"model_name": model_name, "message": "LeCNN all the images"}
+
+  return {"model_name": model_name, "message": "Have some residuals"}
