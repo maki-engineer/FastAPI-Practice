1f54d564bfbdc35211c71ffb2c1827228b2681e4
maki-engineer <ryohei-81@outlook.jp>
2023-04-22 10:32:50 +0900

    update: itemURL変更

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Union
 from fastapi import FastAPI
 
 class ModelName(str, Enum):
@@ -8,13 +9,24 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 @app.get("/")
 def root():
   return {"message": "Hello, World."}
 
+@app.get("/items/")
+async def read_items(skip: int = 0, limit: int = 10):
+  return fake_items_db[skip : skip + limit]
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-  return {"item_id": item_id, "q": q}
+async def read_item(item_id: int, q: Union[str, None] = None, short: bool = False):
+  item = {"item_id": item_id}
+  if q:
+    item.update({"q": q})
+  if not short:
+    item.update({"description": "This is an amazing item that has a long descriptioin."})
+  return item
 
 @app.get("/users/me")
 def read_user_me():
